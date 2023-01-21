1389b2a1fdf4e11329aa25864ae724583d6bc6e6
TanviJhaloya <84829142+TanviJhaloya@users.noreply.github.com>
2023-01-21 20:44:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+    type = string
+    description = "region name"
+    default = "ap-south-1"
+}
+variable "bucket_name" {
+    type = string
+    description = "bucket  name"
+    default = "c-aws-bucket"
+}
